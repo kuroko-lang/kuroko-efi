a23910e8b20de69fe5af65b63bd0d635e6b394c3
K. Lange <klange@toaruos.org>
2022-08-16 15:22:47 +0900

    Trigger SIGINT-like conditions on ^C

--- src/main.c
+++ src/main.c
@@ -17,6 +17,66 @@ extern void free_sbrk_heap(void);
 extern void krkefi_load_module(void);
 extern void _createAndBind_gzipMod(void);
 
+static EFI_GUID efi_simple_text_input_ex_protocol_guid =
+	{0xdd9e7534, 0x7762, 0x4698, {0x8c, 0x14, 0xf5, 0x85, 0x17, 0xa6, 0x25, 0xaa}};
+
+typedef struct EFI_KEY_STATE {
+	UINT32 KeyShiftState;
+	UINT8 KeyToggleState;
+} EFI_KEY_STATE;
+
+typedef struct {
+	EFI_INPUT_KEY Key;
+	EFI_KEY_STATE KeyState;
+} EFI_KEY_DATA;
+
+struct efi_input_ex {
+	void (*Reset)(void);
+	EFI_STATUS (*ReadKeyStrokeEx)(struct efi_input_ex *this, EFI_KEY_DATA *data);
+	void (*WaitForKeyEx)(void);
+	void (*SetState)(void);
+	EFI_STATUS (*RegisterKeyNotify)(struct efi_input_ex *this, EFI_KEY_DATA *data, EFI_STATUS (*func)(EFI_KEY_DATA *data), void **handle);
+	void (*UnregisterKeyNotify)(void);
+};
+
+static EFI_KEY_DATA ctrl_c = {
+	{ 0, 'c' }, { 0x80000008, 0 }
+};
+
+static EFI_STATUS handle_ctrl_c(EFI_KEY_DATA * data) {
+	/* Eat the input if code is executing */
+	if (krk_currentThread.frameCount) {
+		EFI_INPUT_KEY Key;
+		ST->ConIn->ReadKeyStroke(ST->ConIn, &Key);
+	}
+
+	/* Set the signalled state */
+	krk_currentThread.flags |= KRK_THREAD_SIGNALLED;
+	return 0;
+}
+
+static int register_ctrl_callback(void) {
+	UINTN count;
+	EFI_HANDLE * handles;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL * gfx;
+	EFI_STATUS status = ST->BootServices->LocateHandleBuffer(ByProtocol, &efi_simple_text_input_ex_protocol_guid, NULL, &count, &handles);
+
+	if (EFI_ERROR(status)) {
+		return -1;
+	}
+
+	for (UINTN i = 0; i < count; ++i) {
+		struct efi_input_ex * input_ex;
+		void * handle;
+		status = ST->BootServices->HandleProtocol(handles[0], &efi_simple_text_input_ex_protocol_guid, (void **)&input_ex);
+		if (EFI_ERROR(status)) continue;
+		status = input_ex->RegisterKeyNotify(input_ex, &ctrl_c, handle_ctrl_c, &handle);
+		if (EFI_ERROR(status)) continue;
+	}
+
+	return 0;
+}
+
 EFI_STATUS
 	EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
@@ -30,6 +90,8 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 	/* Initialize VM */
 	set_attr(0xF);
 	krk_initVM(0);
+
+	register_ctrl_callback();
 
 	/* Load additional modules */
 	krkefi_load_module();
