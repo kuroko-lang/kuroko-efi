a934ded2dac04642b3c453694ecb974e961ccfda
K. Lange <klange@toaruos.org>
2024-02-16 10:03:50 +0900

    Introduce a better qsort from misaka's libc

--- src/stubs.c
+++ src/stubs.c
@@ -89,21 +89,58 @@ char * strrchr(const char * s, int c) {
 	return NULL;
 }
 
-void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)) {
-	if (!nmemb) return;
-	if (!size) return;
-	for (size_t i = 0; i < nmemb-1; ++i) {
-		for (size_t j = 0; j < nmemb-1; ++j) {
-			void * left = (char *)base + size * j;
-			void * right = (char *)base + size * (j + 1);
-			if (compar(left,right) > 0) {
-				char tmp[size];
-				memcpy(tmp, right, size);
-				memcpy(right, left, size);
-				memcpy(left, tmp, size);
+struct SortableArray {
+	void * data;
+	size_t size;
+	void * arg;
+	int (*func)(const void *, const void *, void *);
+};
+
+static ssize_t partition(struct SortableArray * array, ssize_t lo, ssize_t hi) {
+	char pivot[array->size];
+	memcpy(pivot, (char *)array->data + array->size * hi, array->size);
+	ssize_t i = lo - 1;
+	for (ssize_t j = lo; j <= hi; ++j) {
+		uint8_t * obj_j = (uint8_t *)array->data + array->size * j;
+		if (array->func(obj_j, pivot, array->arg) <= 0) {
+			i++;
+			if (j != i) {
+				uint8_t * obj_i = (uint8_t *)array->data + array->size * i;
+				for (size_t x = 0; x < array->size; ++x) {
+					uint8_t tmp = obj_i[x];
+					obj_i[x] = obj_j[x];
+					obj_j[x] = tmp;
+				}
 			}
 		}
 	}
+	return i;
+}
+
+static void quicksort(struct SortableArray * array, ssize_t lo, ssize_t hi) {
+	if (lo >= 0 && hi >= 0) {
+		if (lo < hi) {
+			ssize_t pivot = partition(array, lo, hi);
+			quicksort(array, lo, pivot - 1);
+			quicksort(array, pivot + 1, hi);
+		}
+	}
+}
+
+static int sort_caller(const void * a, const void * b, void * arg) {
+	int (*func)(const void *, const void *) = arg;
+	return func(a,b);
+}
+
+void qsort_r(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *, void *), void * arg) {
+	if (nmemb < 2) return;
+	if (!size) return;
+	struct SortableArray array = {base,size,arg,compar};
+	quicksort(&array, 0, nmemb - 1);
+}
+
+void qsort(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)) {
+	qsort_r(base,nmemb,size,sort_caller,compar);
 }
 
 static int isspace(int c) {
