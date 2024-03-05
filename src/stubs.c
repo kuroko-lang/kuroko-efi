4ad4ee0b0f2be7f7e9b7a3f530f37d5d2f752f71
K. Lange <klange@toaruos.org>
2024-03-05 13:35:22 +0900

    Slim down unneeded headers

--- src/stubs.c
+++ src/stubs.c
@@ -89,60 +89,6 @@ char * strrchr(const char * s, int c) {
 	return NULL;
 }
 
-struct SortableArray {
-	void * data;
-	size_t size;
-	void * arg;
-	int (*func)(const void *, const void *, void *);
-};
-
-static ssize_t partition(struct SortableArray * array, ssize_t lo, ssize_t hi) {
-	char pivot[array->size];
-	memcpy(pivot, (char *)array->data + array->size * hi, array->size);
-	ssize_t i = lo - 1;
-	for (ssize_t j = lo; j <= hi; ++j) {
-		uint8_t * obj_j = (uint8_t *)array->data + array->size * j;
-		if (array->func(obj_j, pivot, array->arg) <= 0) {
-			i++;
-			if (j != i) {
-				uint8_t * obj_i = (uint8_t *)array->data + array->size * i;
-				for (size_t x = 0; x < array->size; ++x) {
-					uint8_t tmp = obj_i[x];
-					obj_i[x] = obj_j[x];
-					obj_j[x] = tmp;
-				}
-			}
-		}
-	}
-	return i;
-}
-
-static void quicksort(struct SortableArray * array, ssize_t lo, ssize_t hi) {
-	if (lo >= 0 && hi >= 0) {
-		if (lo < hi) {
-			ssize_t pivot = partition(array, lo, hi);
-			quicksort(array, lo, pivot - 1);
-			quicksort(array, pivot + 1, hi);
-		}
-	}
-}
-
-static int sort_caller(const void * a, const void * b, void * arg) {
-	int (*func)(const void *, const void *) = arg;
-	return func(a,b);
-}
-
-void qsort_r(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *, void *), void * arg) {
-	if (nmemb < 2) return;
-	if (!size) return;
-	struct SortableArray array = {base,size,arg,compar};
-	quicksort(&array, 0, nmemb - 1);
-}
-
-void qsort(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)) {
-	qsort_r(base,nmemb,size,sort_caller,compar);
-}
-
 static int isspace(int c) {
 	return c == ' ';
 }
@@ -245,50 +191,6 @@ char * strcpy(char * restrict dest, const char * restrict src) {
 	return out;
 }
 
-double strtod(const char *nptr, char **endptr) {
-	int sign = 1;
-	if (*nptr == '-') {
-		sign = -1;
-		nptr++;
-	}
-
-	long long decimal_part = 0;
-
-	while (*nptr && *nptr != '.') {
-		if (*nptr < '0' || *nptr > '9') {
-			break;
-		}
-		decimal_part *= 10LL;
-		decimal_part += (long long)(*nptr - '0');
-		nptr++;
-	}
-
-	double sub_part = 0;
-	double multiplier = 0.1;
-
-	if (*nptr == '.') {
-		nptr++;
-
-		while (*nptr) {
-			if (*nptr < '0' || *nptr > '9') {
-				break;
-			}
-
-			sub_part += multiplier * (*nptr - '0');
-			multiplier *= 0.1;
-			nptr++;
-		}
-	}
-
-	double expn = (double)sign;
-
-	if (endptr) {
-		*endptr = (char *)nptr;
-	}
-	double result = ((double)decimal_part + sub_part) * expn;
-	return result;
-}
-
 FILE * stdout = NULL;
 FILE * stderr = NULL;
 FILE * stdin  = NULL;
@@ -313,19 +215,6 @@ int puts(const char * s) {
 	fputs(s,stdout);
 	fputc('\n',stdout);
 	return 0;
-}
-
-double frexp(double d, int *exp) {
-	double a;
-	uint64_t in;
-	memcpy(&in, &d, sizeof(double));
-
-	uint64_t out =  in & 0x800fffffffffffffUL;
-	int64_t ex2 = ((in & 0x7ff0000000000000UL) >> 52) - 0x3FE;
-	out |= 0x3fe0000000000000UL;
-	memcpy(&a, &out, sizeof(uint64_t));
-	*exp = ex2;
-	return a;
 }
 
 #define OUT(c) do { callback(userData, (c)); written++; } while (0)
@@ -617,6 +506,7 @@ size_t xvasprintf(int (*callback)(void *, char), void * userData, const char * f
 			case 'g': /* supposed to also support e */
 			case 'f':
 				{
+					/* TODO: This could potentially use the Kuroko float.__format__ */
 					if (precision == -1) precision = 8;
 					double val = (double)va_arg(args, double);
 					uint64_t asBits;
